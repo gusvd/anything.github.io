8f1a2b63555ae82c553a38141467adccf0759d11
Gustavo <59086821+gusvd@users.noreply.github.com>
2020-01-15 13:44:18 +0100

    Update overall-head.js

--- overall-head.js
+++ overall-head.js
@@ -1,15 +1,10 @@
 
 
 // Load Memberstack User Id into Session variable
-
-var LoggedUserID = "" // global variable
-
 MemberStack.onReady.then(function(member) {
 	LoggedUserID = member.id;
 	sessionStorage.setItem("LoggedUserID", LoggedUserID);
 });
-
-console.log(LoggedUserID);
 
 // Add links to NavBar
 var Webflow = Webflow || [];
@@ -17,9 +12,8 @@ Webflow.push(function () {
 	// DOMready has fired
 	// May now use jQuery and Webflow api
 
+	LoggedUserID = sessionStorage.getItem("LoggedUserID");
 	document.getElementById("navbar-profile").href="https://crewmates.webflow.io/users/" + LoggedUserID + "#user-profile";
-  	document.getElementById("navbar-listings").href="https://crewmates.webflow.io/users/" + LoggedUserID;
-
-  
+  	document.getElementById("navbar-listings").href="https://crewmates.webflow.io/users/" + LoggedUserID;  
 });
 
