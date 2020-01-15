4b7e45d4fd0786c977f4fb246b59b20a881d402e
Gustavo <59086821+gusvd@users.noreply.github.com>
2020-01-15 13:58:57 +0100

    Update overall-head.js

--- overall-head.js
+++ overall-head.js
@@ -12,7 +12,7 @@ Webflow.push(function () {
 	// DOMready has fired
 	// May now use jQuery and Webflow api
 	LoggedUserID = sessionStorage.getItem("LoggedUserID");
-	If (LoggedUserID) {
+	if (LoggedUserID) {
 		document.getElementById("navbar-profile").href="https://crewmates.webflow.io/users/" + LoggedUserID + "#user-profile";
   		document.getElementById("navbar-listings").href="https://crewmates.webflow.io/users/" + LoggedUserID;
   	};
