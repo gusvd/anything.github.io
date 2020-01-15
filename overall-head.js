f52fff6ce5ad64d8ad7a0efb1b1dee93e247c7d3
Gustavo <59086821+gusvd@users.noreply.github.com>
2020-01-15 14:48:14 +0100

    Update overall-head.js

--- overall-head.js
+++ overall-head.js
@@ -7,7 +7,9 @@ function GetUserID() {
 			LoggedUserID = member.id;
 			sessionStorage.setItem("LoggedUserID", LoggedUserID);
 			return LoggedUserID;
-		};	
+		} else {
+			return null;
+		};
 	});
 };
 
@@ -18,9 +20,8 @@ var Webflow = Webflow || [];
 Webflow.push(function () {
 	// DOMready has fired
 	// May now use jQuery and Webflow api
-	var test = sessionStorage.getItem("LoggedUserID");
-	if (sessionStorage.getItem("LoggedUserID")) {
-		LoggedUserID = sessionStorage.getItem("LoggedUserID")
+	if (GetUserID()) {
+		LoggedUserID = GetUserID();
 		document.getElementById("navbar-profile").href="https://crewmates.webflow.io/users/" + LoggedUserID + "#user-profile";
   		document.getElementById("navbar-listings").href="https://crewmates.webflow.io/users/" + LoggedUserID;
   	};
