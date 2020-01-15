1778456609c958eee50d0591829031b9e69b8252
Gustavo <59086821+gusvd@users.noreply.github.com>
2020-01-15 14:56:15 +0100

    Update overall-head.js

--- overall-head.js
+++ overall-head.js
@@ -4,22 +4,19 @@
 function GetUserID() {
 	MemberStack.onReady.then(function(member) {
 		if (member.loggedIn) {
-			LoggedUserID = member.id;
-			sessionStorage.setItem("LoggedUserID", LoggedUserID);
-			return LoggedUserID;
+			return member.id;
 		} else {
 			return null;
 		};
 	});
 };
 
-console.log(GetUserID());
-
 // Add links to NavBar
 var Webflow = Webflow || [];
 Webflow.push(function () {
 	// DOMready has fired
-	// May now use jQuery and Webflow api
+	// May now use jQuery and Webflow; api
+	console.log(GetUserID())
 	if (GetUserID()) {
 		LoggedUserID = GetUserID();
 		document.getElementById("navbar-profile").href="https://crewmates.webflow.io/users/" + LoggedUserID + "#user-profile";
