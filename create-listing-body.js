9fa636441cd528f85d7ada864d011a8201ad037b
Gustavo <59086821+gusvd@users.noreply.github.com>
2020-01-17 16:43:27 +0100

    Update create-listing-body.js

--- create-listing-body.js
+++ create-listing-body.js
@@ -101,6 +101,9 @@ function fillInAddress (){
     // Get the place details from the autocomplete object.
     var place = autocomplete.getPlace();
     var country = place.address_components[5].short_name;
-    console.log(country);
+
+    console.log(place);
+    console.log(place.address_components[5].short_name);
+    
     document.getElementById('departure-country').value = country;     
 };
