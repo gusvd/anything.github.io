94d252b7fb4514fb7e1866b59df56f80a8ed9d30
Gustavo <59086821+gusvd@users.noreply.github.com>
2020-01-21 14:49:32 +0100

    Create listing-template-head.js

---
+++ listing-template-head.js
@@ -0,0 +1,23 @@
+
+// Upload care initialize
+
+
+  UPLOADCARE_PUBLIC_KEY = '4c05a24f9e4a4c3b032e';
+  UPLOADCARE_TABS = 'file facebook instagram';
+  UPLOADCARE_EFFECTS = 'crop';
+  UPLOADCARE_IMAGE_SHRINK = '1600x1600';
+  UPLOADCARE_IMAGES_ONLY = true;
+  UPLOADCARE_PREVIEW_STEP = true;
+
+  uploadcare.registerTab('preview', uploadcareTabEffects)
+
+
+UPLOADCARE_LOCALE_TRANSLATIONS = {
+    buttons: {
+      choose: {
+        files: {
+          one: 'Upload image'
+        }
+      }
+    }
+  }
