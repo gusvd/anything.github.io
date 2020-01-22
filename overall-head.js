1c850bfca8e16bbb2cd5e283d6d11f3efb416148
Gustavo <59086821+gusvd@users.noreply.github.com>
2020-01-22 19:03:01 +0100

    Update overall-head.js

--- overall-head.js
+++ overall-head.js
@@ -3,8 +3,8 @@ Webflow.push(function () {
 	MemberStack.onReady.then(function(member) {
 		if (member.loggedIn) {
 			LoggedUserID = member.id;
-			document.getElementById("navbar-profile").href="..//users/" + LoggedUserID + "#user-profile";
-		  	document.getElementById("navbar-listings").href="..//users/" + LoggedUserID;
+			document.getElementById("navbar-profile").href="/users/" + LoggedUserID + "#user-profile";
+		  	document.getElementById("navbar-listings").href="/users/" + LoggedUserID;
 		  	sessionStorage.setItem('LoggedUserID', LoggedUserID);
 		} else {
 			sessionStorage.setItem('LoggedUserID', null);
